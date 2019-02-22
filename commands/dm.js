f0968459e9b3e921b8d4478c8ab9e31bd774e636
Mrgotti <46970832+Mrgotti@users.noreply.github.com>
2019-02-22 14:49:49 +0100

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   
-  const modRole = message.author.id == 289771502432419841;
+  const modRole = message.author.id.find(ID = 289771502432419841);
     if (!modRole)
       return console.log("Le role Mods n'hésite pas!");
       if (!message.member.roles.has(modRole.id))
