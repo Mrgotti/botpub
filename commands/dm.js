71db558d92ef5136a0d24911d773c5daffba6fd3
Mrgotti <46970832+Mrgotti@users.noreply.github.com>
2019-02-25 22:10:03 +0100

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   
-  const modRole = message.guild.roles.find(role => role.name === "lolxd");
+  const modRole = message.guild.roles.find(role => role.name === "omg lol t ki");
 
     if (!modRole)
       return console.log("Le role Mods n'hésite pas!");
