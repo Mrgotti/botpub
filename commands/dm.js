0a250728ff0355b88edff010c32c76a4ee4c084f
Mrgotti <46970832+Mrgotti@users.noreply.github.com>
2019-02-25 23:09:04 +0100

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   
-  const modRole = message.guild.roles.find(role => role.name === "omg lol t ki");
+  const modRole = message.guild.roles.find(role => role.name === "Nitro add");
 
     if (!modRole)
   return message.reply("J'attend ma promotion!!!!");
